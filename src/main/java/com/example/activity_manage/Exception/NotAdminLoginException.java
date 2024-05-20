f09e0b9dcb34470dc506273fbedeb1745258521e
remixxyh <1752095049@qq.com>
2024-05-20 15:07:15 +0800

    新增管理员用户登陆判断

---
+++ src/main/java/com/example/activity_manage/Exception/NotAdminLoginException.java
@@ -0,0 +1,6 @@
+package com.example.activity_manage.Exception;
+
+public class NotAdminLoginException extends BaseException{
+    public NotAdminLoginException(){}
+    public NotAdminLoginException(String msg){super(msg);}
+}
