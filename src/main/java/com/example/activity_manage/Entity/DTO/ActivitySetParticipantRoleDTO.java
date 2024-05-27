84ef5d3dacfe0145e343a90c40f4baa8fa3027e6
remixxyh <1752095049@qq.com>
2024-05-27 15:42:24 +0800

    修复一些已知错误

--- src/main/java/com/example/activity_manage/Entity/DTO/ActivitySetParticipantRoleDTO.java
+++ src/main/java/com/example/activity_manage/Entity/DTO/ActivitySetParticipantRoleDTO.java
@@ -13,5 +13,5 @@ public class ActivitySetParticipantRoleDTO { // 对应userList
     private long managerUid; // 操作者 用户ID
     private long aid; // 活动ID
     private long participantUid; // 参与者uid
-    private String Role; // 参与者角色
+    private String role; // 参与者角色
 }
